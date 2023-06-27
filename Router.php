0ff28d7cb914da6692f20c62a9d5d9ef8600b230
Sarah-Katz <Sarahkatz59@gmail.com>
2023-06-27 14:34:46 +0200

    feat(Router): Added Router

---
+++ Router.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+require_once 'Controllers/UserController.php';
+
+class Router {
+    protected $requestUri;
+    protected $requestMethod;
+
+    public function __construct($requestUri, $requestMethod) {
+        $this->requestUri = $this->removeSubdirectoryFromUri($requestUri, LOCALPATH);
+        $this->requestMethod = $requestMethod;
+    }
+
+    protected function removeSubdirectoryFromUri($uri, $subdirectory) {
+        return substr($uri, strlen($subdirectory));
+    }
+
+    public function route() {
+        // Supprimer les slashes au début et à la fin de l'URI et le diviser en segments
+        $segments = explode('/', trim($this->requestUri, '/'));
+        $controllerName = ucfirst(array_shift($segments)).'Controller';
+        $actionName = strtolower($this->requestMethod).ucfirst(array_shift($segments));
+
+        // Ajouter le namespace complet aux contrôleurs
+        $controllerName = '\\App\\Controllers\\'.$controllerName;
+        // Si le contrôleur n'existe pas ou que la méthode n'existe pas, afficher une erreur 404
+        if (!class_exists($controllerName) || !method_exists($controllerName, $actionName)) {
+            header("HTTP/1.0 404 Not Found");
+            echo "Page not found";
+            exit;
+        }
+
+        // Instancier le contrôleur et appeler la méthode d'action
+        $controller = new $controllerName;
+        call_user_func_array([$controller, $actionName], $segments);
+    }
+}
--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 session_start();
 
+require_once './Router.php';
+
+use App\Router;
+
 // Récupération des variables d'environnement pour la base de données
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -9,3 +13,10 @@ define("DB_HOST", $_ENV['DB_HOST']);
 define("DB_NAME", $_ENV['DB_NAME']);
 define("DB_USER", $_ENV['DB_USER']);
 define("DB_PASS", $_ENV['DB_PASS']);
+define("LOCALPATH", $_ENV['LOCALPATH']);
+
+// Lancement du Router
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$router = new Router($requestUri, $requestMethod);
+$router->route();
