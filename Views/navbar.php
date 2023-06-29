7325beb7f99360cbb80ecf578cf78d6026a3161e
Sarah-Katz <Sarahkatz59@gmail.com>
2023-06-29 14:04:59 +0200

    fix(navbar): integrated navbar in php

--- Views/navbar.php
+++ Views/navbar.php
@@ -1,25 +1,33 @@
-<nav class="navbar blue-background is-flex " role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <a class="navbar-item" href="http://localhost<?php echo LOCALPATH ?>">
-            <img src="http://localhost<?php echo LOCALPATH ?>img/logo.png" height="90">
+<nav class="navbar blue-background is-flex is-justify-content-space-between" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand column is-4 is-flex">
+        <a class="navbar-item" href="http://localhost<?php echo LOCALPATH ?>/post/feed">
+            <img id="logo" src="http://localhost<?php echo LOCALPATH ?>img/logo.png">
         </a>
+        <div class="navbar-item">
+            <span id="logo-name" class="m-0 p-1">Bonnefete</span>
+        </div>
     </div>
 
-<div class="navbar-search">
-    <form class="form">
-        <input class="input" type="text" placeholder="Text input">
-    </form>
-</div>
-</div>
+    <div class="column is-4">
+        <form class="form">
+            <div class="control has-icons-right">
+                <input class="input" type="search" placeholder="Recherchez un utilisateur">
+                <div class="icon is-small is-right">
+                    <i class="fas fa-search"></i>
+                </div>
+            </div>
+        </form>
+    </div>
+    </div>
 
     <div class="navbar-end">
         <div class="navbar-item">
             <div class="buttons">
-                <a class="button is-primary">
-                    <strong>Sign up</strong>
+                <a class="button blue-background button-no-border is-rounded">
+                    <i class="fa-2xl fa-solid fa-user" style="color: white;"></i>
                 </a>
-                <a class="button is-light">
-                    Log in
+                <a class="button is-rounded button-no-border">
+                    <i class="fa-2xl fa-solid fa-right-to-bracket" style="color: #ff0000;"></i>
                 </a>
             </div>
         </div>
