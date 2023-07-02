ed0780d1d2e4f78c70c2b21d0f868d03e5a67553
Sarah-Katz <Sarahkatz59@gmail.com>
2023-07-02 12:33:05 +0200

    feat(like): Added ability to like/dislike posts

--- Views/user/profile.php
+++ Views/user/profile.php
@@ -1,7 +1,12 @@
 <?php
+
+use App\Models\UserModel;
+use App\Models\LikeModel;
+
 require_once 'Views/header.php';
 require_once 'Views/navbar.php';
 ?>
+
 <div class="columns">
     <div class="column is-2 is-offset-1 mt-6" id="user-infos">
         <div class="box blue-background">
@@ -43,10 +48,17 @@ require_once 'Views/navbar.php';
 
 <?php if ($posts) : ?>
     <?php foreach ($posts as $post) : ?>
+        <?php
+        $userModel = new UserModel();
+        $user = $userModel->getUserById($post->getID_user());
+        $likeModel = new LikeModel();
+        $likes = $likeModel->getLikesByPostId($post->getID_post());
+        $likesNumber = $likeModel->countLikesByPostId($post->getID_post());
+        ?>
         <div class="column is-4 is-offset-4">
             <div class="card column">
                 <div class="card-content">
-                    <!-- Condition de propirétaire ou modérateur ou super admin -->
+                    <!-- Condition d'affichage des boutons d'édition-->
                     <?php if ($_SESSION['ID_user'] == $post->getID_user() || $_SESSION['ID_role'] == 2 || $_SESSION['ID_role'] == 3) : ?>
                         <div class="buttons is-pulled-right">
                             <a class="button is-ghost m-0 p-1" href="<?php echo LOCALPATH ?>post/edit/<?= $post->getID_post() ?>"><i class="fas fa-xl fa-pen" style="color: var(--orange);"></i></a>
@@ -57,9 +69,40 @@ require_once 'Views/navbar.php';
                             </form>
                         </div>
                     <?php endif; ?>
+                    <!-- Message -->
                     <div class="content is-large has-border">
                         <?= $post->getMessage() ?>
                     </div>
+                    <!-- Gestion des likes -->
+                    <span class="blue-text is-pulled-left p-1 m-0 has-text-weight-bold"><?= $likesNumber ?></span>
+                    <!-- Vérification, le post est il liké par l'utilisateur connecté ? -->
+                    <?php $hasLiked = false; ?>
+                    <?php foreach ($likes as $like) : ?>
+                        <?php if ($_SESSION['ID_user'] == $like->getID_user()) : ?>
+                            <!-- Si oui, lien de dislike -->
+                            <?php $hasLiked = true; ?>
+                            <a href="<?= LOCALPATH ?>like/dislikePost/<?= $like->getID_like() ?>" class="button is-ghost m-0 p-1 is-pulled-left">
+                                <span class="icon">
+                                    <i class="fa-solid fa-thumbs-down" style="color: var(--blue);"></i>
+                                </span>
+                            </a>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                    <?php if (!$hasLiked) : ?>
+                        <!-- Si non, formulaire caché de like -->
+                        <form action="<?= LOCALPATH ?>like/likePost" method="post">
+                            <input type="hidden" name="ID_post" value="<?= $post->getID_post() ?>">
+                            <input type="hidden" name="ID_user" value="<?= $_SESSION['ID_user'] ?>">
+                            <input type="hidden" name="origin" value="<?= $_SERVER['REQUEST_URI'] ?>">
+                            <button class="button is-ghost m-0 p-1 is-pulled-left" type="submit">
+                                <span class="icon">
+                                    <i class="fa-solid fa-thumbs-up" style="color: var(--blue);"></i>
+                                </span>
+                            </button>
+                        </form>
+                    <?php endif; ?>
+
+                    <!-- Auteur et date -->
                     <h2 class="subtitle is-6 is-pulled-right">Par : <?= $user->getUsername() ?> le <?= $post->getPost_date() ?> </h2>
                 </div>
             </div>
